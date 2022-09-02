d16b816af49d16eddd42d9befbd2d5d9627e1e91
Adon Irani <a@atriskmedia.com>
2022-09-01 23:08:52 -0400

    implement css fade effects

--- src/components/menu.js
+++ src/components/menu.js
@@ -15,34 +15,21 @@ function Menu({ menuPayload, viewportKey, setLispActionHook }) {
     setLispActionHook
   )
   const menuItems = menuItemsRaw && ParseMenuItems(menuItemsRaw)
-  const slogan = (
-    <p>
-      <span>Fortifying the Web since 2002</span>
-    </p>
-  )
+  const slogan = <p>Fortifying the Web since 2002</p>
   const branding = <span>{logo}</span>
-  const contents =
-    viewportKey === "mobile" || viewportKey === "tablet" ? (
-      <>
-        <div className="menu">
-          <div>
-            <div className="menu__branding">{branding}</div>
-            <div className="menu__slogan">{slogan}</div>
-          </div>
-        </div>
-        <div className="menu">
-          <div className="menu__menuitems">{menuItems}</div>
-        </div>
-      </>
-    ) : (
+  const contents = (
+    <>
       <div className="menu">
-        <div className="menu-top">
+        <div>
           <div className="menu__branding">{branding}</div>
           <div className="menu__slogan">{slogan}</div>
         </div>
-        <div className="menu-items menu__menuitems">{menuItems}</div>
       </div>
-    )
+      <div className="menu">
+        <div className="menu__menuitems">{menuItems}</div>
+      </div>
+    </>
+  )
   return (
     <div id="menu-default-container">
       <nav
