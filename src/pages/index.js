57412c8b746a22ab7dca4e3b1b7317a50ef3a28e
Adon Irani <a@atriskmedia.com>
2022-09-05 22:39:29 -0400

    homepage redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,11 @@
-import * as React from "react"
+import { useEffect } from "react"
+import { navigate } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const title = `Home | Tract Stack by At Risk Media`
-
-const IndexPage = () => (
-  <Layout title={title} panesArray={[]} impressions={{}} viewportKey="server">
-    <Seo title={title} />
-    <div>
-      <h1>
-        Welcome to <b>Tract Stack</b>
-      </h1>
-    </div>
-  </Layout>
-)
-
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title={title} />
+const IndexPage = () => {
+  useEffect(() => {
+    navigate("/welcome")
+  }, [])
+  return null
+}
 
 export default IndexPage
