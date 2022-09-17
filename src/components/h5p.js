fe9435cefba10e9a21b310b0c7860da5e6b485be
Adon <a@atriskmedia.com>
2022-09-17 15:43:44 -0400

    h5p embed take two

--- src/components/h5p.js
+++ src/components/h5p.js
@@ -2,28 +2,24 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const H5p = ({ src, title }) => {
-  const handleContentRef = dom => {
-    console.log("listening", dom)
-    if (dom) {
-      dom.onload = () => {
-        console.log("listen")
-        dom?.contentWindow?.H5p?.externalDispatcher?.on(
-          "xAPI",
-          function (event) {
-            console.log("hit")
-            console.log(event.data.statement)
-          }
-        )
-      }
+  const onIframeRef = node => {
+    if (!node) {
+      return
     }
+    node.contentWindow.addEventListener("load", () => {
+      node.contentWindow.H5P.externalDispatcher.on("xAPI", function (event) {
+        console.log(event)
+      })
+      console.log("h5p listener")
+    })
   }
+
   return (
     <iframe
+      ref={onIframeRef}
+      sandbox="allow-same-origin"
       title={title}
-      ref={handleContentRef}
       src={src}
-      frameBorder="0"
-      allowFullScreen="allowfullscreen"
     />
   )
 }
