53dd8c141636d937e94eba7e24660bf4f4d85601
Adon Irani <a@atriskmedia.com>
2022-10-14 01:16:45 -0400

    playing with graph

--- src/components/header.js
+++ src/components/header.js
@@ -8,20 +8,18 @@ const Header = ({ siteTitle }) => {
   const [isExpanded, setIsExpanded] = React.useState(false)
 
   const graphOptions = {
-    highlight: [
-      {
-        class: "Project",
-        property: "name",
-        value: "neo4jd3",
-      },
-      {
-        class: "User",
-        property: "userId",
-        value: "eisman",
-      },
-    ],
-    minCollision: 60,
-    //neo4jDataUrl: "json/neo4jData.json",
+    legend: {
+      StoryFragment: 0,
+      Claim: 1,
+      Impression: 2,
+      Pane: 3,
+      Activity: 4,
+      Visitor: 5,
+      UNLOCKS: 6,
+      CLAIMS: 7,
+      KNOWS: 8,
+      BELIEVES: 9,
+    },
     neo4jData: {
       results: [
         {
@@ -37,41 +35,31 @@ const Header = ({ siteTitle }) => {
                   },
                   {
                     id: "2",
-                    labels: ["ConversionEvents"],
-                    properties: {},
-                  },
-                  {
-                    id: "3",
                     labels: ["SecurityMatters"],
                     properties: {},
                   },
                   {
-                    id: "4",
+                    id: "3",
                     labels: ["AtRiskMediaHelps"],
                     properties: {},
                   },
                   {
-                    id: "5",
+                    id: "4",
                     labels: ["Hero"],
                     properties: {},
                   },
                   {
-                    id: "6",
+                    id: "5",
                     labels: ["Impression"],
                     properties: {},
                   },
                   {
-                    id: "7",
+                    id: "6",
                     labels: ["Activity"],
                     properties: {},
                   },
                   {
-                    id: "8",
-                    labels: ["Welcome"],
-                    properties: {},
-                  },
-                  {
-                    id: "9",
+                    id: "7",
                     labels: ["WelcomeToAtRiskMedia"],
                     properties: {},
                   },
@@ -79,79 +67,51 @@ const Header = ({ siteTitle }) => {
                 relationships: [
                   {
                     id: "1",
-                    type: "SHARES",
-                    startNode: "1",
-                    endNode: "2",
-                    properties: {},
-                  },
-                  {
-                    id: "2",
-                    type: "SHARES",
-                    startNode: "2",
-                    endNode: "3",
-                    properties: {},
-                  },
-                  {
-                    id: "3",
-                    type: "SHARES",
-                    startNode: "1",
-                    endNode: "5",
-                    properties: {},
-                  },
-                  {
-                    id: "4",
-                    type: "SHARES",
-                    startNode: "1",
-                    endNode: "6",
-                    properties: {},
-                  },
-                  {
-                    id: "5",
-                    type: "SHARES",
+                    type: "UNLOCKS",
                     startNode: "1",
                     endNode: "7",
                     properties: {},
                   },
                   {
+                    id: "2",
+                    type: "UNLOCKS",
+                    startNode: "7",
+                    endNode: "5",
+                    properties: {},
+                  },
+                  {
+                    id: "3",
+                    type: "UNLOCKS",
+                    startNode: "7",
+                    endNode: "4",
+                    properties: {},
+                  },
+                  {
+                    id: "4",
+                    type: "UNLOCKS",
+                    startNode: "7",
+                    endNode: "6",
+                    properties: {},
+                  },
+                  {
+                    id: "5",
+                    type: "KNOWS",
+                    startNode: "1",
+                    endNode: "2",
+                    properties: {},
+                  },
+                  {
                     id: "6",
-                    type: "SHARES",
-                    startNode: "5",
-                    endNode: "8",
+                    type: "BELIEVES",
+                    startNode: "1",
+                    endNode: "2",
                     properties: {},
                   },
                   {
                     id: "7",
-                    type: "SHARES",
-                    startNode: "6",
-                    endNode: "8",
-                    properties: {},
-                  },
-                  {
-                    id: "8",
-                    type: "SHARES",
-                    startNode: "7",
-                    endNode: "8",
-                    properties: {},
-                  },
-                  {
-                    id: "9",
-                    type: "SHARES",
-                    startNode: "8",
-                    endNode: "9",
-                    properties: {},
-                  },
-                  {
-                    id: "10",
-                    type: "SHARES",
-                    startNode: "7",
+                    type: "KNOWS",
+                    startNode: "1",
                     endNode: "3",
-                    properties: {},
-                  },
-                  {
-                    id: "11",
-                    type: "SHARES",
-                    startNode: "7",
-                    endNode: "4",
                     properties: {},
                   },
                 ],
@@ -162,13 +122,11 @@ const Header = ({ siteTitle }) => {
       ],
       errors: [],
     },
-    nodeRadius: 25,
     onRelationshipDoubleClick: function (relationship) {
       console.log(
         "double click on relationship: " + JSON.stringify(relationship)
       )
     },
-    zoomFit: true,
   }
 
   return (
