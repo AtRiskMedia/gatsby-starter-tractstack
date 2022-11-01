2760742f4314b3c4472b7352cc90412ce00bc63d
Adon Irani <a@atriskmedia.com>
2022-11-01 16:24:52 -0400

    prettier buttons

--- src/components/controller.js
+++ src/components/controller.js
@@ -32,16 +32,16 @@ const Impression = ({ payload }) => {
   return (
     <>
       <h3 className="text-lg font-medium leading-6 text-gray-900">
-        {payload.altTitle}
+        {payload.title}
       </h3>
       <div className="mt-2 sm:flex sm:items-start sm:justify-between">
         <div className="max-w-xl text-sm text-gray-500">
-          <p>{payload.headline}</p>
+          <p>{payload.body}</p>
         </div>
         <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex sm:flex-shrink-0 sm:items-center">
           <button
             type="button"
-            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+            className="inline-flex items-center rounded-md border border-transparent bg-green px-4 py-2 font-medium text-black shadow-sm hover:bg-orange focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
           >
             {payload.buttonText}
           </button>
