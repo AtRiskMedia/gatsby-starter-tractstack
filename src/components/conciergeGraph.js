2d6f034f42950077bb87942fd2ebb140c106ffc9
Adon Irani <a@atriskmedia.com>
2022-12-27 23:28:55 -0500

    getGraph build data struct

--- src/components/conciergeGraph.js
+++ src/components/conciergeGraph.js
@@ -24,7 +24,7 @@ const getGraph = async fingerprint => {
               {
                 graph: {
                   nodes: graphNodes,
-                  relationships: relationshipNodes,
+                  relationships: graphRelationships,
                 },
               },
             ],
