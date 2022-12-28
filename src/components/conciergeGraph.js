f3c530778a09cdb8a50536d83a74214f9005442a
Adon Irani <a@atriskmedia.com>
2022-12-27 23:37:26 -0500

    getGraph build data struct

--- src/components/conciergeGraph.js
+++ src/components/conciergeGraph.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 
 import { graph } from "../api/services"
 import D3 from "../components/D3"
@@ -44,17 +44,20 @@ const getGraph = async fingerprint => {
 }
 
 const ConciergeGraph = () => {
+  const [graphData, setGraphData] = useState({})
   useEffect(function conciergeGetGraph() {
     getGraph().then(res => {
       console.log(res)
+      setGraphData(res)
     })
   }, [])
-
+  if (graphData && Object.keys(graphData).length === 0) return <></>
+  const thisOptions = { ...graphOptions, neo4jData: graphData }
   return (
     <>
       <p className="p-4">* coming soon; this isn't real data yet!</p>
       <div className="divide-y divide-gray-200 lg:col-span-9 h-screen max-h-[40rem]">
-        <D3 options={graphOptions} slug="conciergeGraph" />
+        <D3 options={thisOptions} slug="conciergeGraph" />
       </div>
     </>
   )
@@ -64,115 +67,7 @@ const graphOptions = {
   distance: 150,
   strength: -350,
   infoPanel: true,
-  labelFontSize: "18px",
-  neo4jData: {
-    results: [
-      {
-        columns: ["user", "entity"],
-        data: [
-          {
-            graph: {
-              nodes: [
-                {
-                  id: "1",
-                  type: "visitor",
-                  labels: ["Visitor"],
-                  properties: {},
-                },
-                {
-                  id: "2",
-                  type: "claim",
-                  labels: ["SecurityMatters"],
-                  properties: {},
-                },
-                {
-                  id: "3",
-                  type: "claim",
-                  labels: ["AtRiskMediaHelps"],
-                  properties: {},
-                },
-                {
-                  id: "4",
-                  type: "pane",
-                  labels: ["Hero"],
-                  properties: {},
-                },
-                {
-                  id: "5",
-                  type: "impression",
-                  labels: ["Impression"],
-                  properties: {},
-                },
-                {
-                  id: "6",
-                  type: "pane",
-                  labels: ["Pane"],
-                  properties: {},
-                },
-                {
-                  id: "7",
-                  labels: ["Activity"],
-                  properties: {},
-                },
-              ],
-              relationships: [
-                {
-                  id: "1",
-                  type: "DISCOVERS",
-                  startNode: "1",
-                  endNode: "4",
-                  properties: {},
-                },
-                {
-                  id: "2",
-                  type: "DISCOVERS",
-                  startNode: "1",
-                  endNode: "6",
-                  properties: {},
-                },
-                {
-                  id: "3",
-                  type: "REVEALS",
-                  startNode: "6",
-                  endNode: "5",
-                  properties: {},
-                },
-                {
-                  id: "4",
-                  type: "REVEALS",
-                  startNode: "6",
-                  endNode: "7",
-                  properties: {},
-                },
-                {
-                  id: "5",
-                  type: "KNOWS",
-                  startNode: "1",
-                  endNode: "2",
-                  properties: {},
-                },
-                {
-                  id: "6",
-                  type: "BELIEVES",
-                  startNode: "1",
-                  endNode: "2",
-                  properties: {},
-                },
-                {
-                  id: "7",
-                  type: "KNOWS",
-                  startNode: "1",
-                  endNode: "3",
-                  properties: {},
-                },
-              ],
-            },
-          },
-        ],
-      },
-    ],
-    errors: [],
-  },
+  labelFontSize: "18px"
 }
 
 export default ConciergeGraph
