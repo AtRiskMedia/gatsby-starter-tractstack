2f14be7fd06c9c47bcdc067a4832db672e976576
Adon Irani <a@atriskmedia.com>
2022-12-28 12:39:23 -0500

    debug session refresh

--- src/pages/{nodeStoryFragment.field_slug}.js
+++ src/pages/{nodeStoryFragment.field_slug}.js
@@ -366,31 +366,31 @@ const RenderedStoryFragment = ({ data }) => {
   const storyFragmentPayload =
     viewportKey !== "server"
       ? storyFragmentCompositor({
-          data: data.nodeStoryFragment,
-          viewportKey: viewportKey,
-          codeHooks: codeHooks,
-          hooks: {
-            updateRevealContext: updateRevealContext,
-            updateContentMap: updateContentMap,
-            processRead: processRead,
-            updateEventStream: updateEventStream,
-            navigate: navigate,
-          },
-        })
+        data: data.nodeStoryFragment,
+        viewportKey: viewportKey,
+        codeHooks: codeHooks,
+        hooks: {
+          updateRevealContext: updateRevealContext,
+          updateContentMap: updateContentMap,
+          processRead: processRead,
+          updateEventStream: updateEventStream,
+          navigate: navigate,
+        },
+      })
       : null
   const tractStackContextPayload =
     viewportKey !== "server" && typeof storyFragmentPayload === "object"
       ? Compositor(
-          data.nodeStoryFragment.relationships.field_tract_stack.relationships
-            .field_context_panes,
-          null,
-          viewportKey,
-          {
-            updateRevealContext: updateRevealContext,
-            updateContentMap: updateContentMap,
-            processRead: processRead,
-          }
-        )
+        data.nodeStoryFragment.relationships.field_tract_stack.relationships
+          .field_context_panes,
+        null,
+        viewportKey,
+        {
+          updateRevealContext: updateRevealContext,
+          updateContentMap: updateContentMap,
+          processRead: processRead,
+        }
+      )
       : null
 
   if (
@@ -450,8 +450,8 @@ const RenderedStoryFragment = ({ data }) => {
         thisWidth < 801
           ? thisWidth / 600
           : thisWidth < 1367
-          ? thisWidth / 1080
-          : thisWidth / 1920
+            ? thisWidth / 1080
+            : thisWidth / 1920
       document.documentElement.style.setProperty("--scale", thisScale * 0.99)
     }
     window.addEventListener("resize", handleResize)
@@ -485,18 +485,19 @@ const RenderedStoryFragment = ({ data }) => {
 
   useEffect(
     function loginToConcierge() {
+      getTokens(fingerprint).then(res => {
+        const accessToken =
+          typeof res.tokens === "string" ? res.tokens : false
+        if (accessToken) {
+          console.log("logged in")
+          login({ accessToken: accessToken, fingerprint: fingerprint })
+          setValidToken(true)
+        } else {
+          console.log("error with token")
+        }
+      })
       if (!validToken && fingerprint > 0) {
-        getTokens(fingerprint).then(res => {
-          const accessToken =
-            typeof res.tokens === "string" ? res.tokens : false
-          if (accessToken) {
-            console.log("logged in")
-            login({ accessToken: accessToken, fingerprint: fingerprint })
-            setValidToken(true)
-          } else {
-            console.log("error with token")
-          }
-        })
+        loginToConcierge()
       }
     },
     [validToken, setValidToken, fingerprint, login]
@@ -507,11 +508,11 @@ const RenderedStoryFragment = ({ data }) => {
     const payload =
       typeof eventStream === "object"
         ? Object.keys(eventStream)
-            .filter(k => k > lastSync)
-            .reduce((obj, key) => {
-              obj[key] = eventStream[key]
-              return obj
-            }, {})
+          .filter(k => k > lastSync)
+          .reduce((obj, key) => {
+            obj[key] = eventStream[key]
+            return obj
+          }, {})
         : {}
     if (isLoggedIn && Object.keys(payload).length > 0) {
       pushPayload({ payload }).then(res => {
