547d35113f6c9fb02bff9fd11d74545950215cca
Adon Irani <a@atriskmedia.com>
2023-01-08 00:10:34 -0500

    beliefs

--- src/components/belief.js
+++ src/components/belief.js
@@ -6,17 +6,13 @@ import { classNames } from "gatsby-plugin-tractstack"
 import { useStoryStepStore } from "../stores/storyStep"
 import { useAuthStore } from "../stores/authStore"
 
-const people = [
-  { id: 1, name: "Wade Cooper", online: true },
-  { id: 2, name: "Arlene Mccoy", online: false },
-  { id: 3, name: "Devon Webb", online: false },
-  { id: 4, name: "Tom Cook", online: true },
-  { id: 5, name: "Tanya Fox", online: false },
-  { id: 6, name: "Hellen Schmidt", online: true },
-  { id: 7, name: "Caroline Schultz", online: true },
-  { id: 8, name: "Mason Heaney", online: false },
-  { id: 9, name: "Claudie Smitham", online: true },
-  { id: 10, name: "Emil Schaefer", online: false },
+const likert = [
+  { id: 0, name: "Agree or Disagree?", color: "bg-slate-50" },
+  { id: 1, name: "Strongly disagree", color: "bg-red-400" },
+  { id: 2, name: "Disagree", color: "bg-amber-400" },
+  { id: 3, name: "Neither agree nor disagree", color: "bg-slate-200" },
+  { id: 4, name: "Agree", color: "bg-lime-400" },
+  { id: 5, name: "Strongly agree", color: "bg-teal-400" },
 ]
 
 const Belief = ({ value, cssClasses }) => {
@@ -26,95 +22,93 @@ const Belief = ({ value, cssClasses }) => {
   const [selected, setSelected] = useState(people[3])
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="block text-sm font-medium text-gray-700">
-            Assigned to
-          </Listbox.Label>
-          <div className="relative mt-1">
-            <Listbox.Button className="relative cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
-              <span className="flex items-center">
-                <span
-                  aria-label={selected.online ? "Online" : "Offline"}
-                  className={classNames(
-                    selected.online ? "bg-green-400" : "bg-gray-200",
-                    "inline-block h-2 w-2 flex-shrink-0 rounded-full"
-                  )}
-                />
-                <span className="ml-3 block truncate">{selected.name}</span>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <ChevronUpDownIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-90000 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map(person => (
-                  <Listbox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? "text-white bg-indigo-600" : "text-gray-900",
-                        "relative cursor-default select-none py-2 pl-3 pr-9"
-                      )
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={classNames(
-                              person.online ? "bg-green-400" : "bg-gray-200",
-                              "inline-block h-2 w-2 flex-shrink-0 rounded-full"
-                            )}
-                            aria-hidden="true"
-                          />
-                          <span
-                            className={classNames(
-                              selected ? "font-semibold" : "font-normal",
-                              "ml-3 block truncate"
-                            )}
-                          >
-                            {person.name}
-                            <span className="sr-only">
-                              {" "}
-                              is {person.online ? "online" : "offline"}
-                            </span>
-                          </span>
-                        </div>
-
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? "text-white" : "text-indigo-600",
-                              "absolute inset-y-0 right-0 flex items-center pr-4"
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
+    <div className={cssClasses}>
+      <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-sm font-medium text-gray-700">
+              Assigned to
+            </Listbox.Label>
+            <div className="relative mt-1">
+              <Listbox.Button className="relative cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-orange focus:outline-none focus:ring-1 focus:ring-orange sm:text-sm">
+                <span className="flex items-center">
+                  <span
+                    aria-label={selected.online ? "Online" : "Offline"}
+                    className={classNames(
+                      selected.online ? "bg-green-400" : "bg-gray-200",
+                      "inline-block h-2 w-2 flex-shrink-0 rounded-full"
                     )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
+                  />
+                  <span className="ml-3 block truncate">{selected.name}</span>
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-90000 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {people.map(likert => (
+                    <Listbox.Option
+                      key={likert.id}
+                      className={({ active }) =>
+                        classNames(
+                          active ? `text-white bg-slate-200` : "text-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9"
+                        )
+                      }
+                      value={likert}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            <span
+                              className={classNames(
+                                likert.color,
+                                "inline-block h-2 w-2 flex-shrink-0 rounded-full"
+                              )}
+                              aria-hidden="true"
+                            />
+                            <span
+                              className={classNames(
+                                selected ? "font-semibold" : "font-normal",
+                                "ml-3 block truncate"
+                              )}
+                            >
+                              {likert.name}
+                            </span>
+                          </div>
+
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? "text-white" : "text-orange",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
+                              )}
+                            >
+                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </div>
   )
 
   //return <span>{value}</span>
