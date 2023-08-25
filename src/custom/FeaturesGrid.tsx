238db903c1686e837cbc306a76787e9cf2407d6d
Adon Irani <a@atriskmedia.com>
2023-08-25 15:04:44 -0400

    remove menuItems for now

--- src/custom/FeaturesGrid.tsx
+++ src/custom/FeaturesGrid.tsx
@@ -5,96 +5,71 @@ import {
   CircleStackIcon,
   ArrowTrendingUpIcon,
   BeakerIcon,
-  ShoppingCartIcon,
 } from '@heroicons/react/24/outline'
 
 import { IViewportKeyProps } from 'gatsby-plugin-tractstack/types'
 
 const FeaturesGrid = ({ viewportKey, storyFragmentId }: IViewportKeyProps) => {
+
+  const features = [
+    {
+      id: `builder`,
+      title: `All-in-one website builder`,
+      description: `Everything your business needs to market and sell online (integrates with Shopify)`,
+      icon: <CursorArrowRaysIcon
+        className="h-10 w-10 absolute left-1 top-2"
+        aria-hidden="true" />
+    },
+    {
+      id: `insights`,
+      title: `Real-time engagement analytics`,
+      description: `As buyers engage with your funnel, we'll generate data-driven insight on what does (and doesn't) convert`,
+      icon: <CircleStackIcon
+        className="h-10 w-10 absolute left-1 top-2"
+        aria-hidden="true"
+      />
+    },
+    {
+      id: `marketing`,
+      title: `Better tooling for content marketing`,
+      description: `Removes all the complexity. Gives you everything you'll need in one tool. You retain ownership and control of your data`,
+      icon: <ArrowTrendingUpIcon
+        className="h-10 w-10 absolute left-1 top-2"
+        aria-hidden="true"
+      />
+    },
+    {
+      id: `qualify`,
+      title: `Allows you to "qualify" your leads`,
+      description: `Easily create interactive 'mini-funnels' that pre-filter or onboard prospective new buyers/clients`,
+      icon: <BeakerIcon
+        className="h-10 w-10 absolute left-1 top-2"
+        aria-hidden="true"
+      />
+    }
+  ]
+
   return (
     <div
       id={`${viewportKey}-${storyFragmentId.id}`}
-      className="sm:px-8 pb-12 bg-allwhite"
+      className="sm:px-8 pb-24 bg-allwhite"
     >
-      <div className="mx-auto max-w-5xl md:max-w-7xl lg:px-8">
-        <div className="relative px-8 lg:px-12">
-          <div className="max-w-3xl md:flex md:flex-row mx-auto w-fit">
-            <dl className="mx-auto max-w-lg">
-              <div className="relative pl-16 pb-8 pt-6">
-                <dt className="inline font-action">
-                  <CursorArrowRaysIcon
-                    className="h-10 w-10 absolute left-1 top-1"
-                    aria-hidden="true"
-                  />
-                  No-code website builder
-                </dt>
-                {` `}
-                <dd className="pt-2">
-                  Get a fast,{` `}
-                  <abbr title="Search Engine Optimization / Search Engine Marketing">
-                    SEO/SEM
-                  </abbr>
-                  {` `}
-                  ready website up in minutes!
-                </dd>
-              </div>
-              <div className="relative pl-16 pb-8">
-                <dt className="inline font-action">
-                  <CircleStackIcon
-                    className="h-10 w-10 absolute left-1 top-1"
-                    aria-hidden="true"
-                  />
-                  Actionable insights + engagement analytics
-                </dt>
-                {` `}
-                <dd className="pt-2">
-                  Customizable dashboards and data-driven insights gleaned from
-                  website activity. Learn from your potential buyers.
-                </dd>
-              </div>
-              <div className="relative pl-16 pb-8">
-                <dt className="inline font-action">
-                  <ArrowTrendingUpIcon
-                    className="h-10 w-10 absolute left-1 top-1"
-                    aria-hidden="true"
-                  />
-                  Passive lead generation
-                </dt>
-                {` `}
-                <dd className="pt-2">
-                  Show buyers the way through their purchasing journey. Help
-                  them understand and &apos;make sense&apos; of each step
-                  towards purchase.
-                </dd>
-              </div>
-              <div className="relative pl-16 pb-8">
-                <dt className="inline font-action">
-                  <BeakerIcon
-                    className="h-10 w-10 absolute left-1 top-1"
-                    aria-hidden="true"
-                  />
-                  Better tooling for content marketing
-                </dt>
-                {` `}
-                <dd className="pt-2">
-                  Everything you need to rank for keywords and to establish
-                  authority in your space.
-                </dd>
-              </div>
-              <div className="relative pl-16 pb-8 pt-6">
-                <dt className="inline font-action">
-                  <ShoppingCartIcon
-                    className="h-10 w-10 absolute left-1 top-1"
-                    aria-hidden="true"
-                  />
-                  E-commerce integration with Shopify
-                </dt>
-                {` `}
-                <dd className="pt-2">
-                  Sell your products direct from your website
-                </dd>
-              </div>
-            </dl>
+      <div className="mx-auto max-w-5xl md:max-w-7xl xl:px-8">
+        <div className="relative px-8 xl:px-12">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            {features.map((feature) => (
+              <div
+                key={feature.id}
+                className="relative flex items-center space-x-3 rounded-lg px-6 py-5 shadow-sm"
+              >
+                <div className="my-auto flex-shrink-0 pr-6">
+                  {feature.icon}
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="text-lg font-action text-black">{feature.title}</p>
+                  <p className="text-md text-darkgrey pt-2">{feature.description}</p>
+                </div>
+              </div>))}
           </div>
         </div>
       </div>
